b2ed9ea0d5ab622d01560149029efa91a1af6a68
RosemarieLopezH <rosemarie61@gmail.com>
2018-08-10 19:01:00 -0500

    test

--- src/js/validation.js
+++ src/js/validation.js
@@ -1,19 +1,5 @@
 expresionCorreo = /\w+@[a-z]+\.+[a-z]/;
 
-<<<<<<< HEAD
-const emailValidation = (email,password)=>{
-  if (email.trim().length === 0 || password.trim().length === 0) {
-    showMessage('dangerMessage', 'Debe de ingresar email y contraseña');
-    return;
-  }  
-  if (!expresionCorreo.test(email)){
-    alert('El correo electrónico no es valido'); 
-    return;   
-  }
-  if (password.length<7){
-    alert('La contraseña debe ser mayor de 7 caracteres'); 
-    return; 
-=======
 const registroValidation = (email,password)=>{
   if (email.length === 0 || password.length === 0) {
     // showMessage('dangerMessage', 'Debe de ingresar email y contraseña');
@@ -26,7 +12,6 @@ const registroValidation = (email,password)=>{
    if (password.length<7){
    //  alert('La contraseña debe ser mayor de 6 caracteres'); 
    return false;
->>>>>>> f9ca99f269ba0cb71d15a2b13dc11e668639328f
   }
   else {
     //registro(email,password);
